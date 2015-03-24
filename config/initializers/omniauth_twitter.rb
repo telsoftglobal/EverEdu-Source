164c9b22feb812b661dc6474de13d2635740f48c
CuongCT <CuongCT@telsoft.com.vn>
2015-03-24 16:00:03 +0700

    2015-03-24

--- config/initializers/omniauth_twitter.rb
+++ config/initializers/omniauth_twitter.rb
@@ -6,10 +6,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :twitter, '9X8Zp9C4RAnc3Tk2gAKzoVNlg', '6QdUNZX9xniD85DeqEngUNKQbn65Ib07iqhAyMSy8KnTPQ8J3G', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 
   #everedu-test
-  provider :twitter, 'y7jMYiNfuTxIhdtt1UIZuobf8', 'lXSeMr5QlRVSF7DEoV5LKLXRuQuUXOZ32UVdZ7puLQ514oJbGg', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  # provider :twitter, 'y7jMYiNfuTxIhdtt1UIZuobf8', 'lXSeMr5QlRVSF7DEoV5LKLXRuQuUXOZ32UVdZ7puLQ514oJbGg', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 
 
-  #everedu-ja
-  # provider :twitter, 'j9ME66ZbHrcFqe3CtaNvV2e0l', 'Jr0Mh51hyMF6uF5wh9x2UGWWjItd2pUVA53utYLIzyCPD6AQaY', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  everedu-ja
+  provider :twitter, 'j9ME66ZbHrcFqe3CtaNvV2e0l', 'Jr0Mh51hyMF6uF5wh9x2UGWWjItd2pUVA53utYLIzyCPD6AQaY', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 
 end
