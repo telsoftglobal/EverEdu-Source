bc0dc1a909afca272dc0e9d55226353cedc93528
CuongCT <CuongCT@telsoft.com.vn>
2015-04-07 09:13:44 +0700

    2015-04-07

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,9 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = session[:locale] || I18n.default_locale
+    if session[:locale].nil?
+      session[:locale] = I18n.default_locale
+    end
     # I18n.locale = I18n.default_locale
   end
 
